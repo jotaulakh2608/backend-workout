e8128e5d6f2e57169995a6757d50b5c19ddb6143
jotaulakh2608 <rattanjotsingh30@gmail.casgit config -- global user.email rattanjotsingh30@gmail.com>
2022-12-10 11:24:57 +0530

    changes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/users')
+const cors = require('cors');
 
 // express app
 const app = express()
@@ -11,6 +12,7 @@ const app = express()
 // middleware
 app.use(express.json())
 
+app.use(cors())
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
